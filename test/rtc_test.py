2c2a9b541dd8e7a248060204ae6480ecbdf4af7b
rockershead <zahir.inspireme@gmail.com>
2021-12-27 17:00:49 +0800

    no message

--- test/rtc_test.py
+++ test/rtc_test.py
@@ -1,10 +1,9 @@
 import json
-from logging import error
 import os
 import os.path
 from os import path
 import requests
-import paho.mqtt.client as mqttClient
+
 from dotenv import load_dotenv
 import json
 from datetime import datetime
@@ -14,47 +13,34 @@ import shutil
 import time
 from datetime import datetime
 
+
 load_dotenv()
 
 
-delete_status=1
+timestamp=datetime.now().isoformat()
+
 url=os.getenv("http_url")
 headers = {'content-type': 'application/json','Authorization':'Bearer '+os.getenv("access_token")}
 
 def send_request(payload):
- error_status=0
  try:
   response = requests.post(url, data=payload, headers=headers)
   print(response.status_code)
   print(response.json())
   if("errors" in response.json() ):
-   error_status=1
+   print("Cant send payload")
+   buffer_file = open("/home/pi/buffer", "a+")
+   buffer_file.write(payload+"\r\n")
+   buffer_file.close()
  except requests.exceptions.RequestException as e:
-  print(e)
-  error_status=1
- 
- return error_status
+  print("Cant send payload")
+  buffer_file = open("/home/pi/buffer", "a+")
+  buffer_file.write(payload+"\r\n")
+  buffer_file.close()
 
 
 
 
-if(path.exists("/home/pi/buffer")):
- file1 = open("/home/pi/buffer", "r")
- lines = file1.readlines()
- for line in lines:
-  error_status=send_request(line.strip())
-  if(error_status==1):
-   delete_status=0
-   break
-  time.sleep(2)
- if (delete_status==1):
-  print("all old data published")
-  os.remove("/home/pi/buffer")
- 
-  
- 
- 
- 
-  						
- 
- 
+json_data={"data":"test","timestamp":timestamp}
+
+send_request(json.dumps(json_data))
